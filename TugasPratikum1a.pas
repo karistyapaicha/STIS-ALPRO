a180dbdd1e2344c57274b5423d85e3cad3bf41db
[2208541024] Ni Putu Karistya Paicha Maesi <127062538+karistyapaicha@users.noreply.github.com>
2024-01-24 08:30:11 +0700

    Add files via upload

---
+++ TugasPratikum1a.pas
@@ -0,0 +1,11 @@
+program TugasPratikum1;
+uses crt;
+begin
+    clrscr;
+    writeln(' ##  ###  ###   ##');
+    writeln('#     #    #   # ');
+    writeln(' #    #    #    #');
+    writeln('  #   #    #     #');
+    writeln('##    #   ###  ##');
+    readkey;
+end.
---
+++ TugasPratikum2a.pas
@@ -0,0 +1,10 @@
+Program TugasPratikum2;
+Uses crt;
+Begin 
+    clrscr;
+    writeln('42');
+    writeln('--- x 52 + 6 X 3 +4');
+    writeln('7');
+    readkey;
+end.
+
